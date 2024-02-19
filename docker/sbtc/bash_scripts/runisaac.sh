b45503b27bb8ee60b0b1674af32755e8ee2cb614
Özhan Özen <oezhan.oezen@sipbb.ch>
2024-02-19 15:10:18 +0100

    Adds custom port option for runisaac.sh

--- docker/sbtc/bash_scripts/runisaac.sh
+++ docker/sbtc/bash_scripts/runisaac.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+# Default port number
+PORT=8211
+
+# Process command-line options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -p|--port)
+            PORT="$2"
+            echo "Starting Isaac sim on custom port $PORT," \
+            "if the port is available. If the port $PORT does not work, " \
+            "check the terminal messages to find the real port number."
+            shift
+            ;;
+        *) 
+            echo "Unknown parameter passed: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
 cd /isaac-sim
 ./runheadless.webrtc.sh \
     --/app/window/dpiScaleOverride=1.0 \
     --/app/livestream/allowResize=true \
     --/app/window/scaleToMonitor=false \
+    --/exts/omni.services.transport.server.http/port=$PORT \
     #--/persistent/app/window/width=2560 \
     #--/persistent/app/window/height=1440
 
